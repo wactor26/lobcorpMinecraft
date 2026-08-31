90ac7d53be92d0ce48b8cbfdf210432e43522c05
wactor26 <corbinbevier@gmail.com>
2026-08-31 17:29:53 -0400

    Update LobCorp.java

--- src/main/java/net/wactor/lobcorp/LobCorp.java
+++ src/main/java/net/wactor/lobcorp/LobCorp.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 
 public class LobCorp implements ModInitializer {
 	public static final String MOD_ID = "lobcorp";
-//yfutfutgiy
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
