3511a7e5545fc775b618a7d924ce3cb9f83eede7
wactor26 <corbinbevier@gmail.com>
2026-08-31 17:29:19 -0400

    Initial commit

---
+++ src/main/java/net/wactor/lobcorp/LobCorpClient.java
@@ -0,0 +1,10 @@
+package net.wactor.lobcorp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LobCorpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/wactor/lobcorp/LobCorpDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wactor.lobcorp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LobCorpDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
