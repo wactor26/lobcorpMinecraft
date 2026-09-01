468a02289c17f7406629823b64caa5c0aa3c5aa9
wactor26 <corbinbevier@gmail.com>
2026-08-31 20:21:15 -0400

    day 1 added one sin weapon ego

---
+++ src/main/java/net/wactor/lobcorp/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.wactor.lobcorp.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.wactor.lobcorp.LobCorp;
+
+public class ModItems {
+
+    public static final Item Penitence_weapon = registerItem("penitence_weapon",new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name,Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(LobCorp.MOD_ID,name),item);
+    }
+    public  static void registerModItem(){
+        LobCorp.LOGGER.info("Registering mod items for"+LobCorp.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(Penitence_weapon);
+        });
+    }
+}
